1e2016c0c1e74a20012ddf5c9cd2ce2e9c00c1e7
wasp <wasp@wasp.local>
2018-02-28 22:36:34 +0900

    svgの隙間を埋める

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import Arranger from './arranger';
 import Piece from './components/piece.jsx'
 
 export default class App extends React.Component {
   render() {
-    const arranger = new Arranger(this.props.state.window_width, this.props.state.window_height)
-    const xys = arranger.call();
-
     return <div>
         <svg version="1.1"
           width={this.props.state.window_width}
           height={this.props.state.window_height}
           xmlns="http://www.w3.org/2000/svg">
-          { xys.map( (xy) => { return <Piece xy={xy} /> }) }
+          <defs>
+            <filter id="polycleaner" color-interpolation-filters="sRGB">
+              <feComponentTransfer>
+                <feFuncA type="table" tableValues="0 0.5 1 1" />
+              </feComponentTransfer>
+            </filter>
+          </defs>
+          <g filter="url(#polycleaner)">
+            { this.props.state.pieces.map( (p) => {
+              return <Piece xy={p.xy} shown={p.shown} opacity={p.opacity} /> }) }
+          </g>
         </svg>
     </div>;
   }
